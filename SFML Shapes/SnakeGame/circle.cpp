8bfa8d383c774e7afd06be9e4bb34bcc0cbc0110
SCOTT <W9036922@live.tees.ac.uk>
2020-02-10 15:40:41 +0000

    Circle Class completed
    user can now define their own circle with radius, position and colour as parameters

---
+++ SFML Shapes/SnakeGame/circle.cpp
@@ -0,0 +1,19 @@
+#include "circle.h"
+#include "SFML/Graphics.hpp"
+
+void Circle::Render(sf::RenderWindow& window) {
+	window.draw(circle);
+}
+
+Circle::Circle() {
+
+}
+
+Circle::Circle(sf::Color colourToBe, const sf::Vector2f vector, float radiusToBe) {
+	colour = colourToBe;
+	position = vector;
+	radius = radiusToBe;
+	circle = sf::CircleShape(radiusToBe);
+	circle.setFillColor(colourToBe);
+	circle.setPosition(vector);
+}
--- SFML Shapes/SnakeGame/circle.h
+++ SFML Shapes/SnakeGame/circle.h
@@ -1,13 +1,16 @@
 #pragma once
 #include "SFML/Graphics.hpp"
 
-class circle
+class Circle
 {
 private:
 	sf::Vector2f position;
 	sf::Color colour;
 	float radius;
+	sf::CircleShape circle;
 public:
 	void Render(sf::RenderWindow& window);
+	Circle();
+	Circle(sf::Color colourToBe, sf::Vector2f vector, float radius);
 };
 
