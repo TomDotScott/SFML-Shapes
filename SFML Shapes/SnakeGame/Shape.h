85c647e0d7e899a0dbc47b9a9bff42b55523df2f
Tom Scott <48756858+TomDotScott@users.noreply.github.com>
2020-02-14 10:13:49 +0000

    Virtual Render
    Because the Rectangle and the Circle both have a render function, the base class can have Render() marked as virtual. This means that the function can be overriden in the child classes

--- SFML Shapes/SnakeGame/Shape.h
+++ SFML Shapes/SnakeGame/Shape.h
@@ -5,6 +5,10 @@ class Shape
 protected:
 	sf::Color colour;
 	sf::Vector2f position;
-
+public:
+	//setting the virtual function to 0 means it is a 'pure' virtual
+	//this means that the function can only be called by its children
+	//so therefore a Shape cannot exist on its own
+	virtual void Render(sf::RenderWindow& window) = 0;
 };
 
